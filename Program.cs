31cbdff9fbe492c6d607152fca270cc689623495
Ilnur Garrapov <garrapovilnur@gmail.com>
2023-01-01 14:38:01 +0300

    68th ex

--- Program.cs
+++ Program.cs
@@ -18,7 +18,7 @@ int InputNumbers(string input)
   int output = Convert.ToInt32(Console.ReadLine());
   return output;
 }
-*/
+
 
 //Задача 66.
 int m = InputNumbers("Введите m: ");
@@ -42,6 +42,29 @@ void PrintSumm(int m, int n, int summ)
     return;
   }
   PrintSumm(m, n - 1, summ);
+}
+
+int InputNumbers(string input) 
+{
+  Console.Write(input);
+  int output = Convert.ToInt32(Console.ReadLine());
+  return output;
+}
+*/
+
+//Задача 68.
+int m = InputNumbers("Введите m: ");
+int n = InputNumbers("Введите n: ");
+
+int functionAkkerman = Ack(m, n);
+
+Console.Write($"Функция Аккермана = {functionAkkerman} ");
+
+int Ack(int m, int n)
+{
+  if (m == 0) return n + 1;
+  else if (n == 0) return Ack(m - 1, 1);
+  else return Ack(m - 1, Ack(m, n - 1));
 }
 
 int InputNumbers(string input) 
